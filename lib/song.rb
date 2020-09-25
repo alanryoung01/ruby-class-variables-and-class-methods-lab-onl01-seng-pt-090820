7323cbb6be4679ffc8e7cb29667bc66224952e54
Alan Young <alanryoung01@gmail.com>
2020-09-25 16:26:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,16 +30,10 @@ def initialize(name, artist, genre)
 end
 
 def self.genre_count
-  genre_count = {}
-  @@genres.each do |genre|
-    if genre_count[genre]
-      genre_count[genre] += 1
-    else
-      genre_count[genre] = 1
-    end
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    genre_count
   end
-  genre_count
-end
 
 def self.artist_count
     artist_count = {}
