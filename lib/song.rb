59c678aff9178b9c36a1a76c3ea3cb39a878aa24
Alan Young <alanryoung01@gmail.com>
2020-09-25 16:18:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,44 +2,46 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
-  @@song_count = 0
-  @@genres = []
-  @@artists = []
+@@song_count = 0
+@@artists = []
+@@genres = []
 
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@genres << @genre
-    @@artists << @artist
-    @@song_count +=1
-  end
 
-  def self.count
-    @@song_count
-  end
 
-  def self.genres
-    @@genres.uniq
-  end
+def self.count
+  @@song_count
+end
 
-  def self.artists
-    @@artists.uniq
-  end
+def self.artists
+  @@artists.uniq
+end
 
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
+def self.genres
+  @@artists.uniq
+end
+
+def initialize(name, artist, genre)
+  @name = name
+  @genre = genre
+  @artist = artist
+  @@genres << @genre
+  @@artists << @artist
+  @@song_count +=1
+end
+
+def self.genre_count
+  genre_count = {}
+  @@genres.each do |genre|
+    if genre_count[genre]
       genre_count[genre] += 1
-      else
+    else
       genre_count[genre] = 1
-      end
     end
-    genre_count
   end
+  genre_count
+end
 
-  def self.artist_count
+def self.artist_count
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist]
@@ -50,5 +52,6 @@ class Song
     end
     artist_count
   end
+
 
 end
